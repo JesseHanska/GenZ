1b911b828866ede78d0afaf2edefd12c6b72593e
Jesse Hanska <jessehanska@ou.edu>
2018-10-10 14:15:11 -0500

    Fixed runtime error, thanks generics

--- GenZ.java
+++ GenZ.java
@@ -6,8 +6,8 @@ public class GenZ
     public static void main(String[] args)
     {
         System.out.println("One day, I will learn Git!!!");
-        Comparable d = new Date();
-        System.out.println(d.compareTo(new String("String")));
+        Comparable<Date> d = new Date();
+        System.out.println(d.compareTo(new Date("2018,1,1")));
         
     }
 
