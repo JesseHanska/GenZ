b50a36b2e5b792883dc55bb351961fae48e8f493
Jesse Hanska <jessehanska@ou.edu>
2018-10-10 14:11:02 -0500

    No generics, no fun

--- GenZ.java
+++ GenZ.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 
 public class GenZ
 {
@@ -5,6 +6,9 @@ public class GenZ
     public static void main(String[] args)
     {
         System.out.println("One day, I will learn Git!!!");
+        Comparable d = new Date();
+        System.out.println(d.compareTo(new String("String")));
+        
     }
 
 }
