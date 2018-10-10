ceb414a58c9db77916aff976995515227d719bb6
Jesse Hanska <jessehanska@ou.edu>
2018-10-10 14:52:46 -0500

    This does not make sense either

--- GenZ.java
+++ GenZ.java
@@ -10,7 +10,10 @@ public class GenZ
         Comparable<Date> d = new Date();
         System.out.println(d.compareTo(new Date(2018,1,1)));
         
+        ArrayList<? extends Number> nlist = new ArrayList<>();
+        ArrayList<? super Integer> xnlist = new ArrayList<>();
         ArrayList<Integer> list = new ArrayList<>();
+        nlist = list;
         //list.add(d);
         list.add(new Integer(4));
         list.add(new Integer(44));
@@ -18,7 +21,7 @@ public class GenZ
         list.add(new Integer(44));
         //list.add(new String("a"));
         Integer sum = 0;
-        for (Number o: list)
+        for (Number o: nlist)
         {
             System.out.println(o);
             sum += (Integer)o;
